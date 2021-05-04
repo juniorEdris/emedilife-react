5b62782aab70dad49fab515cc8a69e6ce9f20539
Eftekar raghib <mdraghib17@gmail.com>
2021-05-04 14:45:22 +0600

    Last day Commit 2 (Feature Area fixed)

--- src/Home/Component/Features.js
+++ src/Home/Component/Features.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import './features.css';
 
 export const Features = (props) => {
   const images = [
@@ -21,17 +22,32 @@ export const Features = (props) => {
     },
   ];
   return (
-    <div className="home_feature_area">
+    <div className="home_feature_area mb-5 mb-md-2 ">
       <div className="container-md-fluid">
-        <div className="row">
-          {images.map((image) => (
+        <div className="features_body">
+          <div className="row">
+            {/* {images.map((image) => (
             <div className="col-6 col-md-3 p-0" key={image.id}>
               <img
                 src={`./assets/images/feature_images/${image.image}`}
                 alt={'feature images'}
               />
             </div>
-          ))}
+          ))} */}
+            {images.map((image) => (
+              <div className="single_feature col-6 col-md-3">
+                <div className="d-flex align-items-center">
+                  <div className="image">
+                    <img
+                      src={`./assets/images/feature_images/genuine_medicine.png`}
+                      alt={'feature images'}
+                    />
+                  </div>
+                  <div className="text">genuine medicines</div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
